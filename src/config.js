8fcdf9e27499b541c2961e16e979ad83c371db15
jianghuawang <46734313+jianghuawang@users.noreply.github.com>
2020-05-08 16:37:32 -0700

    Delete config.js
    protect apikey

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const config={
-    apiKey: "AIzaSyBupURFLwVbel2G9VlwuOQk-BAdDGfSibU",
-    authDomain: "react-portfolio-7cd40.firebaseapp.com",
-    databaseURL: "https://react-portfolio-7cd40.firebaseio.com",
-}
-export default config;
